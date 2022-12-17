860b3266ee9c0e5a7c6362ceb7377bfe2a5bf3e1
alicalilli <113010568+alicalilli@users.noreply.github.com>
2022-12-17 16:36:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wordcounter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.wordcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView =(TextView) findViewById(R.id.textView);
+        editText =(EditText) findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String text = editText.getText().toString();
+                text=text.replace("\n", "");
+                String[]textArray=text.split(" ");
+                textView.setText("Words: " + textArray.length);
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
